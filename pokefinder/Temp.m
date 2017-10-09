af74002fdd72b0f7986567a95b87f8d34e2dad4f
Qiniso <qinisoluthuli@gmail.com>
2017-10-09 06:34:47 +0200

    initial commit

---
+++ pokefinder/Temp.m
@@ -0,0 +1,9 @@
+//
+//  Temp.m
+//  pokefinder
+//
+//  Created by Qiniso Luthuli on 2017/09/04.
+//  Copyright © 2017 Qiniso Luthuli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ pokefinder/pokefinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  pokefinder-Bridging-Header.h
+//  pokefinder
+//
+//  Created by Qiniso Luthuli on 2017/09/04.
+//  Copyright © 2017 Qiniso Luthuli. All rights reserved.
+//
+
+#import "GeoFire.h"
